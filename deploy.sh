35c1edc2b3b0d0fef77dad16a30c8b86ec398b06
Cory Logan <he@corylogan.com>
2017-02-03 15:30:02 -0800

    remove cruft

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 mkdir -p .deploy
 rm -rf .deploy/*
-cp -R homepage/build .deploy/
+cp -R homepage/build/ .deploy/
 cp -R bits/build .deploy/bits
 cp -R old_map/deploy .deploy/map
 aws s3 sync .deploy s3://corylogan-homepage

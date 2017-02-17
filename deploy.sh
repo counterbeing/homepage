376e50ac57c0c18cfeef2d055365591e62d715d8
Cory Logan <he@corylogan.com>
2017-02-17 13:27:03 -0800

    rendered assets exist in avance

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p .deploy
-rm -rf .deploy/*
-cp -R homepage/build/ .deploy/
-cp -R hugo_tech_blog/public .deploy/bits
-cp -R old_map/deploy .deploy/map
-aws s3 sync .deploy s3://corylogan-homepage

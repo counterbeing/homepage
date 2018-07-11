4a0ca5618c9cbff5216d4c01698022535b5f96f7
Cory Logan <he@corylogan.com>
2018-07-10 23:40:11 -0700

    make stylesheets relative, add all steps back in

--- homepage/config.rb
+++ homepage/config.rb
@@ -37,11 +37,9 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
+  activate :relative_assets
 end
 
 require('pry')
